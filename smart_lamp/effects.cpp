508625c780878bf690e4e53d1509146eeb335420
kpzip <kpzipgames@gmail.com>
2024-11-17 01:23:28 -0800

    effects rainbow_wave and retro_bulb_border_strobe

---
+++ smart_lamp/effects.cpp
@@ -0,0 +1,60 @@
+#include <Adafruit_NeoPixel.h>
+#include <stdint.h>
+#include "util.h"
+#include "effects.h"
+#define max_colors 20
+
+// Wave length must not be > max_colors!
+void Effects::rainbow_wave(Adafruit_NeoPixel& strip, int wave_length, long wait, float max_hue=360, float saturation=100.0, float intensity=100.0, bool overflow=false, bool start_small=true) {
+  int np = strip.numPixels();
+
+  uint8_t r_space[max_colors], g_space[max_colors], b_space[max_colors] = {0};
+
+  // Initialize rgb values
+  int hue_step = max_hue/wave_length;
+  float current_hue;
+  for (int i = 0; i < wave_length; i++) {
+    current_hue = hue_step * i;
+    Util::RGBColor color = Util::hsv2rgb(current_hue, saturation, intensity);
+    r_space[i] = color.r;
+    g_space[i] = color.g;
+    b_space[i] = color.b;
+  }
+
+  // Play wave effect
+  for (int i = start_small ? -wave_length : 0; i < np; i++) {
+    if (i - 1 >= 0) {
+      strip.setPixelColor(i-1, 0, 0, 0);
+    }
+
+    for (int j = 0; j < wave_length; j++) {
+      if ((overflow || i + j < np) && i + j >= 0) {
+        strip.setPixelColor((i + j) % np, r_space[j], g_space[j], b_space[j]);
+      }
+    }
+    strip.show();
+    delay(wait);
+    strip.clear();
+    strip.show();
+  }
+}
+
+void Effects::retro_bulb_border_strobe(Adafruit_NeoPixel& strip, long wait, long iterations, Util::RGBColor on_color, Util::RGBColor off_color={ .r = 0, .g = 0, .b = 0 }) {
+  int np = strip.numPixels();
+
+  for (long i = 0; i < iterations; i++) {
+    for (int j = 0; j < np; j++) {
+      if (i % 2 == 0) {
+        strip.setPixelColor(j, on_color.r, on_color.g, on_color.b);
+      }
+      else {
+        strip.setPixelColor(j, off_color.r, off_color.g, off_color.b);
+      }
+    }
+    strip.show();
+    delay(wait);
+    strip.clear();
+    strip.show();
+  }
+}
+
---
+++ smart_lamp/effects.h
@@ -0,0 +1,11 @@
+#include <Adafruit_NeoPixel.h>
+#include "util.h"
+#ifndef EFFECTS_H
+#define EFFECTS_H
+
+namespace Effects {
+  void rainbow_wave(Adafruit_NeoPixel& strip, int wave_length, long wait, float max_hue=360, float saturation=100.0, float intensity=100.0, bool overflow=false, bool start_small=true);
+  void retro_bulb_border_strobe(Adafruit_NeoPixel& strip, long wait, long iterations, Util::RGBColor on_color, Util::RGBColor off_color={ .r = 0, .g = 0, .b = 0 });
+}
+
+#endif
